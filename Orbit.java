fabd997f76b4e407a256741f07b55dfbddb7ddde
ishy848934 <ishamm2828@gmail.com>
2025-09-15 16:52:52 -0400

    Update Orbit.java

--- Orbit.java
+++ Orbit.java
@@ -1,1 +1,10 @@
+public class LunarOrbit {
+    public static void main(String[] args) {
+        double horizontal = 2000; // m/s
+        double vertical = 3000;   // m/s
 
+        double totalSpeedChange = Math.sqrt(horizontal * horizontal + vertical * vertical);
+
+        System.out.println("Total speed change needed: " + totalSpeedChange + " m/s");
+    }
+}
