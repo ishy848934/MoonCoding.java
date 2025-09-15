7a664604226cd20e63b90f5a29c38e4a7e40dfcb
ishy848934 <ishamm2828@gmail.com>
2025-09-15 16:47:56 -0400

    Update Moon.java

--- Moon.java
+++ Moon.java
@@ -1,1 +1,18 @@
-ty
+public class MoonTravel {
+    public static void main(String[] args) {
+        // Distance to the Moon in kilometers
+        double distance = 380000;
+
+        // Speed of the rocket in kilometers per second
+        double speed = 10;
+
+        // Time in seconds (distance ÷ speed)
+        double timeInSeconds = distance / speed;
+
+        // Convert seconds to hours (1 hour = 3600 seconds)
+        double timeInHours = timeInSeconds / 3600;
+
+        // Print the result
+        System.out.println("It would take about " + timeInHours + " hours to reach the Moon.");
+    }
+}
